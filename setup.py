39a1d61ce3afe49f9dcf4cf922b500e2bdc57ffa
hamid reza zare pour <hamidreza.num1@gmail.com>
2020-02-25 16:17:56 +0330

    Add install_requires instead requirements.txt

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import setup
 setup(
     name='app',
     packages=['app'],
-    include_package_data=True
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'connexion[swagger-ui]',
+        'autopep8',
+        'pylint'
+    ],
 )
