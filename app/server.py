e93abfc0c40c15260b2bbeab5c8f08c777310d5f
hamid reza zare pour <hamidreza.num1@gmail.com>
2020-02-26 13:19:45 +0330

    Add notes

--- app/server.py
+++ app/server.py
@@ -1,5 +1,20 @@
+
+"""
+It is our file server that has the task of setting up the API.
+The approach of the structure is a package base.
+We have an app directory that contains our project files.
+use of package model gives us some benefits for
+working with nested files and modules without dependency injection.
+
+The swagger.yml file links the routes to our functions that are placed in the views directory.
+
+for responding to the user requests we use a Response class
+that provides a modular structure. It is based on utils.response_handler.py
+"""
+
 from flask import make_response, abort
 import connexion
+
 
 # Create the application instance
 app = connexion.FlaskApp(
@@ -10,6 +25,7 @@ app = connexion.FlaskApp(
 
 # Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yml', strict_validation=True)
+
 
 # Create a URL route in our application for "/"
 @app.route('/')
